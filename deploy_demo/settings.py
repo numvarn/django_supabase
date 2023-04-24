338f12e978e0e7b97df9f6b973065fe322a0166b
Phisan Sookkhee <phisan.shukkhi@gmail.com>
2023-04-24 11:24:24 +0700

    connect supabase

--- deploy_demo/settings.py
+++ deploy_demo/settings.py
@@ -78,8 +78,12 @@ WSGI_APPLICATION = 'deploy_demo.wsgi.application'
 
 DATABASES = {
     'default': {
-        # 'ENGINE': 'django.db.backends.sqlite3',
-        # 'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'postgres',
+        'USER': 'postgres',
+        'PASSWORD': '8vqvjtnqI3cbK1eR',
+        'HOST': 'db.kitrrqftnveskbhgsmtz.supabase.co',
+        'POST': '5432',
     }
 }
 
